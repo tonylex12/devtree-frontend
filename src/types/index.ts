4df3c54e6d8c157b712c8e8adff14b54aab48ca9
Tony HUerto <tonylex12@gmail.com>
2024-12-25 11:30:30 -0500

    ADD: LoginView with validations

--- src/types/index.ts
+++ src/types/index.ts
@@ -8,3 +8,7 @@ export type RegisterForm = Pick<User, "name" | "email" | "handle"> & {
   password: string;
   password_confirmation: string;
 };
+
+export type LoginForm = Pick<User, "email"> & {
+  password: string;
+};
--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,9 +1,89 @@
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { isAxiosError } from "axios";
+import { toast } from "sonner";
+import ErrorMessage from "../components/ErrorMessage";
+import api from "../config/axios";
+import { LoginForm } from "../types";
 
 const LoginView = () => {
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>({
+    defaultValues: initialValues,
+  });
+
+  const handleLogin = async (formData: LoginForm) => {
+    try {
+      const { data } = await api.post("/auth/login", formData);
+      toast.success(data.message);
+    } catch (error) {
+      if (isAxiosError(error) && error.response) {
+        toast.error(error.response.data.error);
+      }
+    }
+  };
+
   return (
     <>
       <h1 className="text-white text-4xl font-bold">Iniciar Sesión</h1>
+
+      <form
+        onSubmit={handleSubmit(handleLogin)}
+        className="bg-white px-5 py-20 rounded-lg space-y-10 mt-10"
+        noValidate
+      >
+        <div className="grid grid-cols-1 space-y-3">
+          <label htmlFor="email" className="text-2xl text-slate-500">
+            E-mail
+          </label>
+          <input
+            id="email"
+            type="email"
+            placeholder="Email de Registro"
+            className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+            {...register("email", {
+              required: "El Email es obligatorio",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "E-mail no válido",
+              },
+            })}
+          />
+          {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
+        </div>
+        <div className="grid grid-cols-1 space-y-3">
+          <label htmlFor="password" className="text-2xl text-slate-500">
+            Password
+          </label>
+          <input
+            id="password"
+            type="password"
+            placeholder="Password de Registro"
+            className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+            {...register("password", {
+              required: "El Password es obligatorio",
+            })}
+          />
+          {errors.password && (
+            <ErrorMessage>{errors.password.message}</ErrorMessage>
+          )}
+        </div>
+
+        <input
+          type="submit"
+          className="bg-cyan-400 p-3 text-lg w-full uppercase text-slate-600 rounded-lg font-bold cursor-pointer"
+          value="Iniciar Sesión"
+        />
+      </form>
+
       <nav className="mt-10">
         <Link
           to="/auth/register"
