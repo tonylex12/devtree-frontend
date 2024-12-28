a56f7263621ecdd40e937e6f345941cdb2849bc5
Tony HUerto <tonylex12@gmail.com>
2024-12-28 07:23:06 -0500

    ADD: axios interceptor

--- src/api/DevTreeAPI.ts
+++ src/api/DevTreeAPI.ts
@@ -2,13 +2,8 @@ import { isAxiosError } from "axios";
 import api from "../config/axios";
 
 export async function getUser() {
-  const token = localStorage.getItem("AUTH_TOKEN");
   try {
-    const { data} = await api.get("/user", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
+    const { data} = await api.get("/user")
     return data;
   } catch (error) {
     if(isAxiosError(error) && error.response) {
