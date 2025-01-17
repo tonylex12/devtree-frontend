a9d00610fd0a3525859bad84a9a5df1663ea558d
Tony HUerto <tonylex12@gmail.com>
2025-01-16 21:23:14 -0500

    ADD: Switch to enable social media links

--- src/router.tsx
+++ src/router.tsx
@@ -20,5 +20,5 @@ export default function Router() {
         </Route>
       </Routes>
     </BrowserRouter>
-  );
+  );                 
 }
--- src/views/LinkTreeView.tsx
+++ src/views/LinkTreeView.tsx
@@ -5,10 +5,36 @@ import DevTreeInput from "../components/DevTreeInput";
 const LinkTreeView = () => {
   const [devTreeLinks, setDevTreeLinks] = useState(social);
 
+  const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    const updatedLinks = devTreeLinks.map((item) => {
+      if (item.name === name) {
+        return { ...item, url: value };
+      }
+      return item;
+    });
+    setDevTreeLinks(updatedLinks);
+  };
+
+  const handleEnableLink = (name: string) => {
+    const updatedLinks = devTreeLinks.map((item) => {
+      if (item.name === name) {
+        return { ...item, enabled: !item.enabled };
+      }
+      return item;
+    });
+    setDevTreeLinks(updatedLinks);
+  };
+
   return (
     <div className="space-y-5">
       {devTreeLinks.map((item) => (
-        <DevTreeInput key={item.name} item={item} />
+        <DevTreeInput
+          key={item.name}
+          item={item}
+          handleUrlChange={handleUrlChange}
+          handleEnableLink={handleEnableLink}
+        />
       ))}
     </div>
   );
